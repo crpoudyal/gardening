3d5031b66d160ed4c779b0e7957ffd7b32012c4f
ajitaz <ajtialbert.ab@gmail.com>
2020-05-25 18:43:33 +0545

    edit search bar

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
-
-            
+//placeholder typewriter effect...
 const search = document.getElementById('test');
-var i =0;
-var message='What are You Looking For';
-var speed=100;
-search.addEventListener('click',()=>{
-   
-    type();
+
+var i = 0;
+var message = 'What are You Looking For';
+search.addEventListener('click', () => {
+// type();
+    setTimeout(type,1000);
 })
-function type(){
-    if(i < message.length){
-        msg = search.getAttribute('placeholder')+ message.charAt(i); 
-        search.setAttribute('placeholder',msg);
+
+function type() {
+    if (i < message.length) {
+        msg = search.getAttribute('placeholder') + message.charAt(i);
+        search.setAttribute('placeholder', msg);
         i++;
-        setTimeout(type,speed);
+        setTimeout(type, 50);
     }
 }
+
+//display "Hit enter" when key typed on search bar.....
+const tip = document.getElementById('tip');
+search.addEventListener('keydown',() => {
+    tip.style.visibility= "visible";
+    tip.style.opacity="1";
+})
+
+
+//search bar animation...
+$(document).ready(function () {
+    $('.search').click(function () { //select your searchbar by class and onclick create a new function
+        var my_searchbar = $(this); //my_searchbar is how you can access your searchbar
+
+        my_searchbar.animate({ marginTop: '150px' }, 1000);
+    });
+});
+
+
