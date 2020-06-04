050a044ab45b242cecbf5423c897697c6fbd6fab
ajitaz <ajtialbert.ab@gmail.com>
2020-06-04 21:40:26 +0545

    Responsive Post-slider

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
 });
 
 
-// ...................
+// post slider.........................
 
 $('.category').slick({
     slidesToShow: 3,
@@ -46,5 +46,33 @@ $('.category').slick({
     autoplaySpeed: 2000,
     nextArrow: $('.next'),
     prevArrow: $('.prev'),
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
   });
                   
