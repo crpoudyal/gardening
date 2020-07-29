d196a91139b2ac6103d259e1598c4b07d7e5ec4d
ajitaz <ajtialbert.ab@gmail.com>
2020-07-29 21:58:23 +0545

    NavBar effects using JS

--- js/main.js
+++ js/main.js
@@ -75,4 +75,15 @@ $('.category').slick({
         // instead of a settings object
       ]
   });
-                  
+     
+  
+  //.........................navBar sticky effects...............
+  
+  window.addEventListener("scroll",()=>{
+    var navBar = document.querySelector("nav");
+    var navBarsec = document.querySelectorAll("nav ul li a")
+    navBar.classList.toggle("sticky",window.scrollY > 0);
+    for(var i = 0; i<navBarsec.length;i++){
+      navBarsec[i].classList.toggle("sticky",window.scrollY > 0);
+    }
+  })
